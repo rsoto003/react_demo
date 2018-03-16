8140e622a8d5526c5ec0cef8b529469f65648612
Ryan Soto <rsoto003@ucr.edu>
2018-03-16 14:17:50 -0700

    react demo finished.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app';
 
+
+const user = {
+    firstName: 'Ryan',
+    lastName: 'Soto'
+}
+
+function greetUser(user){
+    return user.firstName + ' ' + user.lastName;
+}
+const greeting = <h1>Hello {greetUser(user)} </h1>;
+
 ReactDOM.render(
-    <App />,
+    greeting,
+    document.getElementById('root')
+);
+
     document.getElementById('root')
 );
